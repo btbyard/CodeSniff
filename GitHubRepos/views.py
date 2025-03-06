4af3ff9c883b587095889dd27dd20e082457c1ab
Rehanashri <rehgr@umich.edu>
2025-03-06 17:51:43 -0500

    basic Register and Login page created

--- GitHubRepos/views.py
+++ GitHubRepos/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .registerForm import RegisterForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])  # Hash password
+            user.save()
+            messages.success(request, "Account created successfully! Please log in.")
+            return redirect("login")  # Change 'login' to your login page URL name
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home') 
+            else:
+                form.add_error(None, 'Invalid username or password')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
---
+++ theme/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './templates/**/*.html',
+      './static/js/**/*.js',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
