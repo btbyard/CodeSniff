936f6b74362f6e98abe1ea3a81fb74b56074292e
btfuhrma <btfuhrma@mtu.edu>
2025-03-06 16:42:10 -0500

    Initial models for the db, db tables havent been migrated yet incase changes are needed

--- GitHubRepos/models.py
+++ GitHubRepos/models.py
@@ -7,3 +7,11 @@ class GitHubRepos:
     githubID = models.AutoField(primary_key=True) # auto incrementing id for primary key
     repositoryName = models.CharField(max_length=255) # Repo name field
     user = models.ForeignKey(User, on_delete=models.CASCADE) # which user it is connected to
+
+class CodeSmellResult(models.Model):
+    gitHubRepo = models.OneToOneField(GitHubRepos, on_delete=models.CASCADE) # Relation for mapping repo to the smell results
+    codeSmellFile = models.FileField(upload_to='code_smell_results/') # File upload location for the results (Assuming XML Format possibly). Mby use XML tree to generate and read the results
+
+class CodeCoverageResult(models.Model):
+    gitHubRepo = models.OneToOneField(GitHubRepos, on_delete=models.CASCADE) # Relation for mapping repo to the smell results
+    codeCoverageFile = models.FileField(upload_to='coverage_results/') # File upload locations for the results (Assuming XML Format possibly). Mby use XML tree to generate and read the results
